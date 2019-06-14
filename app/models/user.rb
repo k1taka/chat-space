5970253359807cade851a7fa394efb015f661ccb
k1taka <sofmori@gmail.com>
2019-06-14 20:18:37 +0900

    make view of sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :group
-  belongs_to :user
+
+  has_many :messages
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
