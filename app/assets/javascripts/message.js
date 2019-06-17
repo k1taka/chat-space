201242e0f34d8d92e3db0888a72aff3002e771d6
k1taka <sofmori@gmail.com>
2019-06-17 17:34:24 +0900

    receive json by done &append HTML
    返ってきたJSONをdoneメソッドで受取り、HTMLを作成。作成したHTMLをメッセージ画面の一番下に追加する。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="message__uper-info">
+                    <p class="message__uper-info__talker">
+                      ${message.name}
+                    </p>
+                    <p class="message__uper-info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    </p><p class="message__text__content">
+                      ${message.content}
+                    </p>
+                    <img class="message__text__image" src="${@message.image}">
+
+                </div>`
+
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,5 +31,13 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-  })
-})
+     })
+      .done(function(message){
+        var html = buildHTML(message);
+        $('.messages').append(html)
+        $('#message_content').val('')
+      })
+      .fail(function(){
+      })
+  })  
+});
