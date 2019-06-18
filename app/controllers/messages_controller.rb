a90c3bd947cdf6738d14b0d2adc166a199540eb4
k1taka <sofmori@gmail.com>
2019-06-18 16:32:31 +0900

    update Pointed out
    プルリクエストで指摘をもらった箇所の修正
    ・不要なファイルの削除
    ・インデントの変更と不要な文言の削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,12 +10,10 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
 
       if @message.save
-      respond_to do |format|
-      format.html { redirect_to message_path(params[:message_id])  }
-      format.json
-      end
-      else
-
+        respond_to do |format|
+          format.html { redirect_to message_path(params[:message_id])  }
+          format.json
+        end
       end
   end
 
--- db/migrate/20190618054300_add_message_to_hugarails.rb
+++
@@ -1,7 +0,0 @@
-class AddMessageToHugarails < ActiveRecord::Migration[5.0]
-  def change
-
-    # change_column_null :messages, :image, false
-
-  end
-end
