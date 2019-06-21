723c60a2da0b423b7fc66e09970cc7578ab8069d
k1taka <sofmori@gmail.com>
2019-06-21 15:04:07 +0900

    create api/messagescontroller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,16 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_group
 
   def index
-    @messages = @group.messages.includes(:user).where.not()
+    respond_to do |format|
+      format.json{@messages = @group.messages.where('id > ?',params[:id])}
+    end
   end
+
+  private
+
+  def set_group
+    @group = Group.find(paramas[:group_id])
+  end
+
 end
-
-
